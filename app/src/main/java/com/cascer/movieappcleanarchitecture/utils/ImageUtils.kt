a61138fdba267e40dd7c2e465045c2869d7c5415
AkhmadGhafirin <ahmadghafirin@gmail.com>
2023-11-10 16:57:14 +0700

    remove glide module cache strategy

--- app/src/main/java/com/cascer/movieappcleanarchitecture/utils/ImageUtils.kt
+++ app/src/main/java/com/cascer/movieappcleanarchitecture/utils/ImageUtils.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Color
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.cascer.movieappcleanarchitecture.R
 import com.cascer.movieappcleanarchitecture.utils.Constant.IMAGE_W500_URL
 import java.util.Random
@@ -12,6 +13,7 @@ object ImageUtils {
     fun ImageView.load(context: Context, url: String) {
         Glide.with(context)
             .load(IMAGE_W500_URL + url.trim())
+//            .diskCacheStrategy(DiskCacheStrategy.ALL)
             .into(this)
     }
 
@@ -19,6 +21,7 @@ object ImageUtils {
         Glide.with(context)
             .load(IMAGE_W500_URL + url.trim())
             .placeholder(R.drawable.baseline_account_circle_24)
+//            .diskCacheStrategy(DiskCacheStrategy.ALL)
             .circleCrop()
             .into(this)
     }
